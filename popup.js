0b4f154fd8be67b116127c838a304d234856293b
sergio <sergio.marsella@gmail.com>
2020-12-27 23:42:25 +0100

    add permission to hydrodaten and parse last line

--- popup.js
+++ popup.js
@@ -1,1 +1,13 @@
 document.getElementById("currentOutflow").innerHTML = "191";
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://www.hydrodaten.admin.ch/graphs/2018/discharge_2018.csv", true);
+xhr.onreadystatechange = function() {
+  if (xhr.readyState == 4) {
+    var allTextLines = xhr.responseText.split(/\r\n|\n/);
+    var lastLine = allTextLines[allTextLines.length - 2];
+    var discharge = lastLine.split(",")[1];
+    document.getElementById("currentOutflow").innerHTML = Math.round(discharge);
+  }
+}
+xhr.send();
